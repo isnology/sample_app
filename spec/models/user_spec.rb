3481fbb6becf0f0d462d35a2a59e24dc384942c8
Glenn <glenn@isnology.com>
2014-07-08 09:58:39 +1000

    Default Change List

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,5 +12,41 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = User.new(name: "Example User", email: "user@example.com") }
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should be_valid }
+
+  describe "when name is not present" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
+  describe "when name is too long" do
+    before { @user.name = "a" * 51 }
+    it { should_not be_valid }
+  end
+
+  describe "when email is blank (not nil)" do
+    before { @user.email = " " }
+    it { should_not be_valid }
+  end
+  describe "when email format -" do
+    it "is invalid" do
+      %w[user@foo,com user_at_foo.org example.user@foo.
+         foo@bar_baz.com foo@bar+baz.com].each do |invalid_address|
+         @user.email = invalid_address
+        should_not be_valid
+      end
+    end
+
+    it "is valid" do
+      %w[user@foo.com A_US-ER@f.b.org
+         first.last@foo.jp a+b@baz.cn].each do |valid_address|
+        @user.email = valid_address
+        should be_valid
+      end
+    end
+  end
 end
