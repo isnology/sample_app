e1fc90a2fd0cdc1467cff4cae496054081b4a14d
Glenn <glenn@isnology.com>
2014-07-08 00:12:59 +1000

    Default Change List

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,8 +9,7 @@ describe "Static Pages" do
     before { visit root_path }
 
     it { should have_selector('h1', text: 'Sample App') }
-    it { should have_selector('title',
-                  text: "#{base_title}") }
+    it { should have_selector('title', text: full_title('')) }
     it { should_not have_selector('title', text: '| Home') }
   end
 
@@ -18,26 +17,20 @@ describe "Static Pages" do
     before { visit help_path  }
 
     it { should have_selector('h1', text: 'Help') }
-    it { should have_selector('title',
-                                text: "#{base_title}") }
-    it { should_not have_selector('title', text: '| Help') }
+    it { should have_selector('title', text: full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path }
 
     it { should have_selector('h1', text: 'About Us') }
-    it { should have_selector('title',
-                                  text: "#{base_title}") }
-    it { should_not have_selector('title', text: '| About Us') }
+    it { should have_selector('title', text: full_title('About Us')) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
     it { should have_selector('h1', text: 'Contact') }
-    it { should have_selector('title',
-                                text: "#{base_title}") }
-    it { should_not have_selector('title', text: '| Contact') }
+    it { should have_selector('title', text: full_title('Contact')) }
   end
 end
