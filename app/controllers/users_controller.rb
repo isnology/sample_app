e9171f4b3a937bfb876bad08e5303b1da0c94f50
Glenn <glenn@isnology.com>
2014-07-10 02:43:41 +1000

    Default Change List

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      # a successful save has occured.
+      render 'show'
     else
       render 'new'
     end
