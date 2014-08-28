9d44f01d441744929a84fa8897553dbcae35b392
Glenn <glenn@isnology.com>
2014-08-28 14:27:42 +1000

    Default Change List

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    name     "Glenn Marks"
-    email    "glenn@isnology.com"
+    sequence(:name)   { |n| "Person #{n}" }
+    sequence(:email)  { |n| "Person_#{n}@example.com" }
     password "foobar"
     password_confirmation "foobar"
   end
