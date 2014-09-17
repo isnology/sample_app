1f5f692d84b584dcaa116379ef0c6b783bf1b7e2
Glenn <glenn@isnology.com>
2014-09-17 18:04:20 +1000

    Default Change List

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,7 @@
 #  updated_at      :datetime        not null
 #  password_digest :string(255)
 #  remember_token  :string(255)
+#  admin           :boolean(1)      default(FALSE)
 #
 
 require 'spec_helper'
@@ -26,6 +27,7 @@ describe User do
   it { should respond_to(:remember_token) }
   it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:microposts)}
   it { should be_valid }
   it { should_not be_admin }
 
@@ -106,5 +108,28 @@ describe User do
     before { @user.toggle!(:admin) }
 
     it { should be_admin }
+  end
+
+  describe "micropost association" do
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right micrposts in the right order" do
+      @user.microposts.should == [newer_micropost, older_micropost]
+    end
+
+    it "should destroy associated microposts" do
+      microposts = @user.microposts
+      @user.destroy
+      microposts.each do |micropost|
+        Micropost.find_by_id(micropost.id).should be_nil
+      end
+    end
+
   end
 end
