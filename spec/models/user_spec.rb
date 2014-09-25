6c9d07dd0b2be14e56bb5cbeb348746e4e8358d8
Glenn <glenn@isnology.com>
2014-09-25 19:18:10 +1000

    Default Change List

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -154,7 +154,7 @@ describe User do
       its(:feed) { should include(older_micropost) }
       its(:feed) { should_not include(unfollowed_post) }
       its(:feed) do
-        followed_user.micoposts.each do |micropost|
+        followed_user.microposts.each do |micropost|
           should include(micropost)
         end
       end
